ee0d0fdcfad3a2844641b91d402c9c01e92623e7
Stuart Sierra <mail@stuartsierra.com>
2013-08-01 10:08:04 -0400

    Add clojure.reflect to user ns

--- src/leiningen/new/reloaded/templates/user.clj
+++ src/leiningen/new/reloaded/templates/user.clj
@@ -5,6 +5,7 @@
    [clojure.java.io :as io]
    [clojure.java.javadoc :refer (javadoc)]
    [clojure.pprint :refer (pprint)]
+   [clojure.reflect :refer (reflect)]
    [clojure.repl :refer (apropos dir doc find-doc pst source)]
    [clojure.set :as set]
    [clojure.string :as str]
