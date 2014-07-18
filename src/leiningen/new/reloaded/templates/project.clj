7b67b17db9a16ea500c71eb0780ffd6d5880fa0b
Stuart Sierra <mail@stuartsierra.com>
2014-07-18 11:00:52 -0400

    Update template to Clojure 1.6 & tools.namespace 0.2.5

--- src/leiningen/new/reloaded/templates/project.clj
+++ src/leiningen/new/reloaded/templates/project.clj
@@ -3,6 +3,6 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5"]]
                    :source-paths ["dev"]}})
