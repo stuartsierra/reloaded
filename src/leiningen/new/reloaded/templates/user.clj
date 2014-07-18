4d38fda0731d016bacbf59a2daff4412e1594c64
Stuart Sierra <mail@stuartsierra.com>
2014-07-18 11:01:29 -0400

    Use square brackets for ns :refer
    This style is becoming more common

--- src/leiningen/new/reloaded/templates/user.clj
+++ src/leiningen/new/reloaded/templates/user.clj
@@ -3,14 +3,14 @@
   not be included in a production build of the application."
   (:require
    [clojure.java.io :as io]
-   [clojure.java.javadoc :refer (javadoc)]
-   [clojure.pprint :refer (pprint)]
-   [clojure.reflect :refer (reflect)]
-   [clojure.repl :refer (apropos dir doc find-doc pst source)]
+   [clojure.java.javadoc :refer [javadoc]]
+   [clojure.pprint :refer [pprint]]
+   [clojure.reflect :refer [reflect]]
+   [clojure.repl :refer [apropos dir doc find-doc pst source]]
    [clojure.set :as set]
    [clojure.string :as str]
    [clojure.test :as test]
-   [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+   [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [{{main-ns}}]))
 
 (def system
