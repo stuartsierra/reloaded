54d487eddfcce4528e2210e34aea8178cc92197c
Stuart Sierra <mail@stuartsierra.com>
2014-07-18 11:16:34 -0400

    project.clj: 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject reloaded/lein-template "0.1.2-SNAPSHOT"
+(defproject reloaded/lein-template "0.1.2"
   :description "A Leiningen 2 template to generate project skeletons using
   tools.namespace and a :dev profile with a user.clj file."
   :url "https://github.com/stuartsierra/reloaded"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true)
