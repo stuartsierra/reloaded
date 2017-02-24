7c308be447579a91b4c5cbcaa27a6e7150078147
Stuart Sierra <mail@stuartsierra.com>
2017-02-24 09:34:17 -0500

    Update Clojure version in template to 1.8.0

--- src/leiningen/new/reloaded/templates/project.clj
+++ src/leiningen/new/reloaded/templates/project.clj
@@ -3,6 +3,6 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}})
