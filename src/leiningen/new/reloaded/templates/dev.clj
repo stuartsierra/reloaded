a09a5e9e2ca9f1b12e282e306a529dacae7062b0
Stuart Sierra <mail@stuartsierra.com>
2017-02-24 09:35:16 -0500

    Rename clojure.string alias to 'string'

--- src/leiningen/new/reloaded/templates/dev.clj
+++ src/leiningen/new/reloaded/templates/dev.clj
@@ -8,7 +8,7 @@
    [clojure.reflect :refer [reflect]]
    [clojure.repl :refer [apropos dir doc find-doc pst source]]
    [clojure.set :as set]
-   [clojure.string :as str]
+   [clojure.string :as string]
    [clojure.test :as test]
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [{{main-ns}}]))
