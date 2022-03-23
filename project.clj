9046352c65f448ccd727191abc5318d0d566352c
Stuart Sierra <mail@stuartsierra.com>
2022-03-23 16:08:11 -0400

    Version 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded/lein-template "1.1.0"
+(defproject reloaded/lein-template "1.1.1-SNAPSHOT"
   :description "A Leiningen 2 template to generate project skeletons using
   tools.namespace and a :dev profile with a dev.clj file."
   :url "https://github.com/stuartsierra/reloaded"
