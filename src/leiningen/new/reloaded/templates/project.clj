4166de0c4fc87c3a7d219ea021f6af7eb4596c53
Stuart Sierra <mail@stuartsierra.com>
2022-03-23 15:32:49 -0400

    Update dependencies of generated project

--- src/leiningen/new/reloaded/templates/project.clj
+++ src/leiningen/new/reloaded/templates/project.clj
@@ -4,7 +4,7 @@
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [com.stuartsierra/component.repl "0.2.0"]]
+                 [com.stuartsierra/component "1.1.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.2.0"]
+                                  [com.stuartsierra/component.repl "1.0.0"]]
                    :source-paths ["dev"]}})
