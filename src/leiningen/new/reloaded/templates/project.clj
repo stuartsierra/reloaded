b8810c7bfaf30956768bd3d3534d10d409a692bc
Alessandra Sierra <105090644+lambdasierra@users.noreply.github.com>
2025-10-25 16:22:45 -0400

    Update Clojure to 1.12.3 in template

--- src/leiningen/new/reloaded/templates/project.clj
+++ src/leiningen/new/reloaded/templates/project.clj
@@ -3,8 +3,8 @@
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
                  [com.stuartsierra/component "1.1.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                                   [com.stuartsierra/component.repl "1.0.0"]]
                    :source-paths ["dev"]}})
