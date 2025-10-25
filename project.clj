7c1d57e3ba13158e1f54dc475fea799020ef0806
Alessandra Sierra <105090644+lambdasierra@users.noreply.github.com>
2025-10-25 16:39:16 -0400

    Set development version 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reloaded/lein-template "1.1.1-SNAPSHOT"
+(defproject reloaded/lein-template "1.2.0-SNAPSHOT"
   :description "A Leiningen 2 template to generate project skeletons using
   tools.namespace and a :dev profile with a dev.clj file."
   :url "https://github.com/stuartsierra/reloaded"
